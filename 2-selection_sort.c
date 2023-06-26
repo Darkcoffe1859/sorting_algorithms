3e9b30faf21460c6d65e325f8693a47682d78615
Darkcoffe1859 <shency49@gmail.com>
2023-06-26 04:14:42 -0700

    mehn

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,47 @@
 #include <sort.h>
-
 /**
- * selection_sort - sort an array in ascending order using the selection method
- * @size: The size of the array
- * @array: the array of the integer
- *
- * Return: Void
+ * print_array - print an array
+ * @size: size of the array
  */
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++) {
+        if (i != 0)
+            printf(", ");
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+/**
+ * selection_sort - sort an array of integer in ascending order 
+ * using selection sort algorithm
+ * @array: the array of the sorted integer
+ * @size: size of the sorted array
+ */
+
 void selection_sort(int *array, size_t size)
 {
-	unsigned long int i = 0, j = 0, smallest = 0;
-	int *swap = NULL;
+    size_t i, j, min_idx;
 
-	if (!array || size < 2)
-		return;
-	for (; i < size; i++)
-	{
-		smallest = i;
-		swap =  malloc(sizeof(int) * 1);
-		*swap = *(array + i);
-		for (j = i + 1; j < size; j++)
-		{
-			if (*(array + j) < *(array + smallest))
-			{
-				smallest = j; 
-			}
-		}
-		if (i < smallest)
-		{
-		*(array + i) = *(array + smallest);
-		*(array + smallest) = *swap;
-		free(swap);
-		print_array(array, size);
-		}
-		else
-			free(swap);
-	}
+    for (i = 0; i < size - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < size; j++) {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i) {
+            swap(&array[i], &array[min_idx]);
+            print_array(array, size);
+        }
+    }
 }
